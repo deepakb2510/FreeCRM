67920d81753262280a12cb793223abec403e7764
deepakb2510 <deepbhan23@gmail.com>
2022-03-16 12:42:16 +0530

    Added LoginTest

---
+++ Features/Login.feature
@@ -0,0 +1,36 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+@tag
+Feature: Verification of Login page functionality
+  I want to use this template for my feature file
+
+Background: User is on FreeCRM page 
+
+  @tag1
+  Scenario: To validate if the user has entered the valid credentials and redirection happens to the home page.
+    Given User is on Login page
+    And User enters username with <"<username>"> 	
+    And User enters password with <"<password>">
+    And User Clicks on login button
+    Then UseR is logged in
+ 
+    Examples: 
+      |username|password|   
+      |deepak.bhandare.48@gmail.com|Deepak@2000|  
+       
---
+++ src/test/java/com/stepDefinition/CompaniesTest.java
@@ -0,0 +1,47 @@
+package com.stepDefinition;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class CompaniesTest {
+	public static WebDriver driver;
+@Given("User is on Login page")
+public void user_is_on_login_page() {
+	System.setProperty("webdriver.chrome.driver", "E:\\Capgemini training\\drivers\\chromedriver_win32\\chromedriver.exe");
+	driver=new ChromeDriver();
+	driver.navigate().to("https://ui.cogmento.com/");
+	driver.manage().window().maximize();
+	
+}
+
+@Given("User enters username with <{string}>")
+public void user_enters_username_with(String string) throws InterruptedException {
+	 Thread.sleep(5000);
+     driver.findElement(By.xpath("/html/body/div[1]/div/div/form/div/div[1]/div/input")).sendKeys(string);
+}
+
+@Given("User enters password with <{string}>")
+public void user_enters_password_with(String string) {
+	 
+    driver.findElement(By.name("password")).sendKeys(string);
+}
+
+@Given("User Clicks on login button")
+public void user_clicks_on_login_button() {
+	driver.findElement(By.xpath("//*[@id=\"ui\"]/div/div/form/div/div[3]")).click();
+}
+
+@Then("UseR is logged in")
+public void use_r_is_logged_in() throws InterruptedException {
+    assertEquals(driver.getTitle(),"Cogmento CRM");
+    Thread.sleep(5000);
+}
+
+
+}
